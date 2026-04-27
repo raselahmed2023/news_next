f739009415cf9a4580b299a28091484fa85ff8af
Rasel Ahmed <raselahmediu22@gmail.com>
2026-04-27 10:05:20 +0600

    data is fetching now

---
+++ src/app/(main)/category/[id]/page.jsx
@@ -0,0 +1,42 @@
+import LeftSideBar from "@/components/News/LeftSideBar";
+import RightSideBar from "@/components/News/RightSideBar";
+
+async function getCategories() {
+    const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
+    const data = await res.json();
+    return data.data;
+}
+
+async function getNewsByCategories({ category_id }) {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`);
+    const data = await res.json();
+    return data.data;
+}
+
+export default async function CategoryNews({ params }) {
+    const { id } = await params;
+
+    const categories = await getCategories();
+    const news = await getNewsByCategories({ category_id: id });
+
+    return (
+        <div className="grid grid-cols-12 mx-auto container mt-10 gap-4">
+            <div className="col-span-3">
+                <h2 className="font-bold text-2xl">All Category</h2>
+                <LeftSideBar categories={categories} />
+            </div>
+
+            <div className="col-span-6">
+                <h2 className="font-bold text-2xl">Dragon News Home</h2>
+                {news.map(n => (
+                    <div key={n._id}>{n.title}</div>
+                ))}
+            </div>
+            
+            <div className="col-span-3">
+                <h2 className="font-bold text-2xl">Find Us On</h2>
+                <RightSideBar />
+            </div>
+        </div>
+    );
+}
--- src/components/News/LeftSideBar.jsx
+++ src/components/News/LeftSideBar.jsx
@@ -1,16 +1,20 @@
+import Link from 'next/link';
 import React from 'react';
 
-const LeftSideBar = ({categories}) => {
+const LeftSideBar = ({ categories, activeId }) => {
     return (
         <div>
             <ul className="flex flex-col gap-3 mt-2">
-          {
-            categories.news_category.map(c => {
-              return <li key={c.category_id} className="bg-slate-100 p-2 rounded-md text-center text-gray-600">{c.category_name}</li>
-            })
-          }
-        </ul>
-            
+                {
+                    categories.news_category.map(c => {
+                        return <li key={c.category_id} className={`${activeId === c.category_id && "bg-slate-300"} p-2 rounded-md text-gray-600`}>
+
+                            <Link href={`/category/${c.category_id}`} className='block'> {c.category_name}</Link>
+                        </li>
+                    })
+                }
+            </ul>
+
         </div>
     );
 };
